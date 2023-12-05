d62fb07731cda0a7616139ad5281369a419c94e3
s0n1krules123 <peachyymomo@gmail.com>
2023-12-05 20:24:15 +0800

    Created homepage

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
+}
+
+function closeSidebar() {
+  document.getElementById('sidebar').style.width = '0';
+}
